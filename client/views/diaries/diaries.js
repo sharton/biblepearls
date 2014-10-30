160c45b99b9f8070749b74a16e9d3148e09272a2
Kazakov Ruslan <ruslan.kazakov@gmail.com>
2014-10-30 17:46:40 +0600

    diaries pages updated

--- client/views/diaries/diaries.js
+++ client/views/diaries/diaries.js
@@ -1,5 +1,4 @@
 Template.diaries.rendered = function() {
-
 };
 
 Template.userDiaryBox.helpers({
@@ -11,10 +10,14 @@ Template.userDiaryBox.helpers({
 });
 
 Template.systemDiaryDetails.events({
-  'click #btn-diary-add' : function() {
+  'click #btn-diary-add' : function(event, template) {
+    event.preventDefault();
+    alert(this._id);
+    var userDiaryName = template.find("#userDiaryName").value;
     UserDiaries.insert({
       user_id: Meteor.userId(),
-      diary_id: this._id
+      diary_id: this._id,
+      user_diary_name: userDiaryName
     });
     Router.go('diaries');
   }
